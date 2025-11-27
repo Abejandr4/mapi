4c08e37ed15ed8e073d8372e1dd5e5ace7513e0f
Jesus17632 <jesusflores.tech@gmail.com>
2025-11-27 16:16:15 -0600

    Application V1

---
+++ MAPI V2/MAPI V2Tests/MAPI_V2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MAPI_V2Tests.swift
+//  MAPI V2Tests
+//
+//  Created by iOS Lab on 27/11/25.
+//
+
+import Testing
+@testable import MAPI_V2
+
+struct MAPI_V2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
